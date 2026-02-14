db2e3501287376133ea0292c491f6e7d2f8db344
lechuga44286-ops <lechuga44286@gmail.com>
2026-02-13 20:01:53 -0500

    Create config.php with general application configuration

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// General Application Configuration
+
+// Set the default timezone
+date_default_timezone_set('UTC');
+
+// Error handling configuration
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Other general settings can be added here.
